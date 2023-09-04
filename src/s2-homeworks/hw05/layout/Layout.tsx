c6484ec2ae80ae2ab5e09ffa33a318b207175254
Vladimir Zvyagin <vladimir_zvyagin@mail.ru>
2023-09-04 11:54:44 +0300

    fixed move elements with open sidebar add paddingRight to body 17px hw5

--- src/s2-homeworks/hw05/layout/Layout.tsx
+++ src/s2-homeworks/hw05/layout/Layout.tsx
@@ -1,6 +1,6 @@
-import React, { FC, ReactNode, useEffect, useState } from 'react'
-import { Header } from '../header/Header'
-import { Sidebar } from '../sidebar/Sidebar'
+import React, {FC, ReactNode, useEffect, useState} from 'react'
+import {Header} from '../header/Header'
+import {Sidebar} from '../sidebar/Sidebar'
 
 type PropsType = {
     children: ReactNode
@@ -12,8 +12,13 @@ export const Layout: FC<PropsType> = ({ children }) => {
     const handleOpen = () => setOpen(true)
 
     useEffect(() => {
-        open && (document.body.style.overflow = 'hidden')
-        !open && (document.body.style.overflow = 'unset')
+        if (open) {
+            document.body.style.overflow = 'hidden'
+            document.body.style.paddingRight = '17px'
+        } else {
+            document.body.style.overflow = 'unset'
+            document.body.style.paddingRight = '0px'
+        }
     }, [open]) // отключает прокрутку при открытом меню
 
     return (
