7df9c3484296f5de344f0d43576f65f745b82ae7
Vladimir Zvyagin <vladimir_zvyagin@mail.ru>
2023-10-23 14:56:49 +0300

    hw10 completed

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,11 +1,17 @@
+
+
 const initState = {
-    isLoading: false,
+    isLoading: false as boolean,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: LoadingActionType): typeof initState=> { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
-
+        case "CHANGE_LOADING":
+            return  {
+                ...state,
+                isLoading: action.isLoading
+            }
         default:
             return state
     }
