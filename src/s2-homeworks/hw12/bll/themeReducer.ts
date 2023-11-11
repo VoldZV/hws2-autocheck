82fdf52ce2c7b06ceb9b97a12fcdf4dbf0e845c0
Vladimir Zvyagin <vladimir_zvyagin@mail.ru>
2023-11-11 18:26:43 +0300

    hw12 completed

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,18 @@
-const initState = {
+export const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state = initState, action: ReturnType<typeof changeThemeId>): typeof initState => { // fix any
     switch (action.type) {
         // дописать
-
+        case 'SET_THEME_ID':
+            return {
+                ...state,
+                themeId: action.id,
+            }
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number) => ({ type: 'SET_THEME_ID', id } as const) // fix any
