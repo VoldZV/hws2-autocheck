a5a12525c5f4d7c0453f2980015e310aab186dce
Vladimir Zvyagin <vladimir_zvyagin@mail.ru>
2023-12-03 13:03:46 +0300

    fix hw13 completed

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -35,19 +35,23 @@ const HW13 = () => {
         axios
             .post(url, {success: x})
             .then((res) => {
+                debugger
                 setCode('Код 200!')
                 setImage(success200)
                 // дописать
-                setInfo('')
+                setText(res.data.errorText)
+                setInfo(res.data.info)
             })
             .catch((e) => {
                 // дописать
+                debugger
                 const  err = e as AxiosError
                 const reqStatus = err.response?.status
                 const errImage = reqStatus === 400 ? error400 : reqStatus === 500 ? error500 : errorUnknown
                 setCode(`Код ${reqStatus}!`)
                 setImage(errImage)
-                setInfo('')
+                setInfo(err.code || 'some error')
+                setText(err.message)
             })
     }
 
